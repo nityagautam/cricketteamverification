4b8d04b6b7cc8b6a110cd8be34c39da2ecab99d3
Nitya Narayan Gautam <nityanarayan44@live.com>
2023-01-27 13:49:36 +0530

    updated values

--- src/test/java/config/OtherConstants.java
+++ src/test/java/config/OtherConstants.java
@@ -6,12 +6,12 @@ package config;
  * @created 25 Jan 2023
  * @lastModified 25 Jan 2023
  * @desc 
- * 		Have all the universal configuration and constants values
+ * 		Have all the universal constants values
  */
 public class OtherConstants {
 	
 	// Declaring some static constant/final members
 	public static final int DEFAULT_WAIT_TIME = 10000;
-	public static final int ONE_MILLI_SEC = 1000;
+	public static final int ONE_SEC_IN_MILLIS = 1000;
 	
 }
